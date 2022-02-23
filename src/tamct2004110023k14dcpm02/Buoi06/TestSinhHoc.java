381163d7500f1a4377714992e22118290befec72
ChauThanhTam2004110023 <tamc5985@gmail.com>
2022-02-23 16:00:23 +0700

    them class

---
+++ src/tamct2004110023k14dcpm02/Buoi06/TestSinhHoc.java
@@ -0,0 +1,11 @@
+package tamct2004110023k14dcpm02.Buoi06;
+
+public class TestSinhHoc {
+    public static void main(String[] args) {
+        SinhVien sv1 = new SinhVien("tâm", 200, 8, 8);
+        SinhVien sv2 = new SinhVien("tâm", 200, 8, 8);
+        System.out.println(sv1);
+        System.out.println(sv2);
+    }
+}
+
